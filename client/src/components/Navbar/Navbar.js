ac3d700a892908372d070694e29edff018b8d515
DocErnstz <ernesto800@outlook.com>
2021-08-30 21:39:58 -0300

    mainbar + form modal fixed

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -19,7 +19,8 @@ const Navbar = () => {
   }, [location]);
 
   return (
-    <nav class="navbar shadow navbar-expand-lg navbar-light bg-light sticky-top" style={{width: "100vw"}, {padding: ".5rem 0"}}>
+    <div>
+       <nav class="navbar shadow navbar-expand-lg navbar-light bg-light sticky-top" style={{width: "100vw", padding: ".5rem 0"}}>
       <div class="container-fluid"> 
         <a class="navbar-brand m-0 p-2" href="#"
           ><i class="fab fa-reddit-alien fa-2x"></i></a>
@@ -42,7 +43,7 @@ const Navbar = () => {
             </div>
           </form>
           <div class="d-flex">
-           <div class="d-flex align-items-center p-2"><a href="">
+           <div class="d-flex align-items-center p-2"><a href="" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
              <i class="far fa-user fa-2x"></i>
              </a></div>
            <div class="d-flex flex-column">
@@ -53,6 +54,50 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
+    <div class="collapse position-fixed fixed-right" style={{width: "220px", right: "0px"}} id="collapseExample">
+  <div class="card card-body" style={{margin: "0px"}}>
+    <button class="btn btn-primary block w-100 border-0" data-bs-toggle="modal" data-bs-target="#exampleModal">SignUp</button>
+    <button class="btn border-primary block w-100 mt-2">Login</button>
+  </div>
+</div>
+    <div class="modal fade" id="exampleModal" style={{overflow: "hidden"}} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog mx-auto h-100 my-0 d-flex align-items-center" style={{"max-width": "800px"}}>
+    <div class="modal-content">
+      <div class="d-flex">
+        <div class="bg-primary" style={{"flex-basis": "200px"}}>
+          <div  style={{height: "500px", width: "100%"}} id="imgForm"></div>
+        </div>        
+         <div class="d-flex flex-column flex-grow-1 p-3">
+         <div class="d-flex justify-content-between">
+           <h3 class="flex-grow-1">Login</h3>
+                   <button type="button" class="border-0" data-bs-dismiss="modal" aria-label="Close">
+                     <i class="fas fa-times fa-2x"></i>
+                   </button>
+         </div>
+         <div class="d-flex h-100">
+           <div id="forms" class="d-flex flex-column justify-content-end mb-5">
+              <div class="form-floating mb-3">
+                <label for="floatingInput">Email address</label>
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+              
+            </div>
+            <div class="form-floating">
+              <label for="floatingPassword">Password</label>
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
+              
+            </div>
+            <a href="" class="btn btn-primary mt-2 rounded-pill block w-100 border-0">SignIn</a>
+           </div>
+         </div>
+      </div>
+      
+      </div>
+     
+    </div>
+  </div>
+</div>
+    </div>
+   
   );
 };
 
--- client/src/components/Search/search.js
+++ client/src/components/Search/search.js
@@ -17,22 +17,115 @@ const Searchbar = () => {
   const prefix = "r/";
   const space = "/";
   return (
-    <div className="container">
-      <div className="row align-items-center">
-        <div className="col">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search"
-            onChange={(e) => setKey(e.target.value)}
-            autoFocus
-          />
-        </div>
+    <div class="container" id="Mainbar" style={{overflowX: "hidden"}}>
+      
+                    
+      <div class="row">
+        <div id="homePosts" class="col-12 col-md-7">
+           <div class="d-flex flex-column h-100">
+             <div class="slice bg-white mb-4 rounded shadow">
+               <div class="container h-100">
+                 <div class="d-flex h-100 align-items-center">
+                   <div class="d-flex justify-content-center" style={{"flex-basis": "50px"}}>
+                    <i class="far fa-user fa-2x"></i>
+                   </div>
+                   <div class="flex-grow-1">
+                     <input type="text" placeholder="Create Post" class="w-100 p-1 rounded"/>
+                   </div>
+                 </div>
+               </div>
+             </div>
+             <div class="slicePost bg-white  mb-4 rounded shadow">
+              
+                 <div class="row p-3 h-100">
+                 <div class="col-1" id="votes">
+                     <i class="fas fa-chevron-up"></i>
+                   <div class="text-center">99</div>
+                    <i class="fas fa-chevron-down"></i>
+                 </div>
+                 <div class="col-11">
+                   <div class="d-flex flex-column h-100" id="ContentPost">
+                     <div class="d-flex align-items-center">
+                         <div class="sub mr-1"></div>
+                        <div class="sub_title"> 
+                          r/Planets</div>
+                       </div>
+                     <div class="h5 fw-bolder">Seekin out new planets geek friends</div>
+                     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                       Sequi similique officiis voluptatem, 
+                       facere illum numquam exercitationem corporis delectus, 
+                       temporibus alias odio molestias 
+                       praesentium aspernatur est laboriosam? At consectetur quam reprehenderit.
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit aliquid eos nulla. Exercitationem, veritatis modi voluptates quisquam accusantium quasi natus a odio officia? Perspiciatis natus quis eos amet reprehenderit laudantium.
+                      </div>
+                     <div class="flex-grow-1 d-flex align-items-center">
+                       <i class="far fa-comment fa-2x"></i>
+                       <div class="fw-bolder ml-1">99 Comments</div>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+               
+               
+             </div>
+              <div class="slicePost bg-white  mb-4 rounded shadow">
+              
+                 <div class="row p-3 h-100">
+                 <div class="col-1" id="votes">
+                     <i class="fas fa-chevron-up"></i>
+                     <div className="text-center">99</div>
+                      <i class="fas fa-chevron-down"></i>
+                 </div>
+                 <div class="col-11">
+                   <div class="d-flex flex-column h-100" id="ContentPost">
+                     <div class="d-flex align-items-center">
+                         <div class="sub mr-1"></div>
+                        <div class="sub_title"> 
+                          r/Planets</div>
+                       </div>
+                     <div class="h5 fw-bolder">Seekin out new planets geek friends</div>
+                     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                       Sequi similique officiis voluptatem, 
+                       facere illum numquam exercitationem corporis delectus, 
+                       temporibus alias odio molestias 
+                       praesentium aspernatur est laboriosam? At consectetur quam reprehenderit.
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit aliquid eos nulla. Exercitationem, veritatis modi voluptates quisquam accusantium quasi natus a odio officia? Perspiciatis natus quis eos amet reprehenderit laudantium.
+                      </div>
+                     <div class="flex-grow-1 d-flex align-items-center">
+                       <i class="far fa-comment fa-2x"></i>
+                       <div class="fw-bolder ml-1">99 Comments</div>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+               
+               
+             </div>
+
+            
+           </div>
+          </div>
+         
+        
+         <div id="infoReddit" class="col-12 h-50 shadow col-md-4 mx-auto bg-white rounded">
+        
+            
+           <div class="bg-white" style={{height: "250px"}}>
+              
+               <div class="h-100 p-3 container d-flex flex-column justify-content-between">
+                 <p>
+                   Your personal Reddit frontpage. Come here to check in with your favorite communities.
+                 </p>
+                 <div class="btn btn-outline bg-primary text-white block w-100 border-0">Create Post</div>
+                 <div class="btn btn-outline border-primary bg-white text-primary mt-2 block w-100 ">Create Community</div>
+                 </div>
+            
+             
+             </div>
+             
+           </div>
+         </div>
       </div>
-      {filter_subs.map((sub) => (
-        <SubCard sub={sub} key={uuidv4()} />
-      ))}
-    </div>
   );
 };
 
